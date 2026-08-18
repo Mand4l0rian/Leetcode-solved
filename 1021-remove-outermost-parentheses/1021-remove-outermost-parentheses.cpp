f9142741ce3d2683d0fab801ecbe26a1190b2607
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-08-18 15:25:13 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.85%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,29 +1,53 @@
+// self attempt first string question (bad tc and sc due to usage of stack)
+
+// class Solution {
+// public:
+//     string primconv(string& temp){
+//         string converted;
+//         if(temp.length()==2) return converted;
+//         converted=temp.substr(1,temp.length()-2);
+//         return converted;
+//     }
+//     string removeOuterParentheses(string s) {
+//         int len=s.length();
+//         stack<char> stack;
+//         string ans;
+//         string temp;
+//         for(int i=0;i<len;i++){
+//             if(stack.empty()){
+//                 temp=(s[i]);
+//                 stack.push(s[i]);
+//             }else{
+//                 temp.push_back(s[i]);
+//                 if(s[i]=='(') stack.push(s[i]);
+//                 else stack.pop();
+//                 if(stack.empty()){
+//                     ans=ans+primconv(temp);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
-    string primconv(string& temp){
-        string converted;
-        if(temp.length()==2) return converted;
-        converted=temp.substr(1,temp.length()-2);
-        return converted;
-    }
     string removeOuterParentheses(string s) {
-        int len=s.length();
-        stack<char> stack;
         string ans;
-        string temp;
-        for(int i=0;i<len;i++){
-            if(stack.empty()){
-                temp=(s[i]);
-                stack.push(s[i]);
-            }else{
-                temp.push_back(s[i]);
-                if(s[i]=='(') stack.push(s[i]);
-                else stack.pop();
-                if(stack.empty()){
-                    ans=ans+primconv(temp);
-                }
+        int depth = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                if (depth > 0)
+                    ans.push_back(c);
+                depth++;
+            }
+            else {
+                depth--;
+                if (depth > 0)
+                    ans.push_back(c);
             }
         }
+
         return ans;
     }
 };
