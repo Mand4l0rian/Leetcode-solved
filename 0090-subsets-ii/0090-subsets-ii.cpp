c8e113e97cd0550e9feae13868398b3696bbe53b
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-08-28 15:43:49 +0530

    Time: 0 ms (100%), Space: 10.8 MB (39.36%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,45 @@
+// original approach using sets to remove duplicates
+// class Solution {
+// public:
+//     set<vector<int>> s;
+//     void solve(int ind, vector<int>& nums, vector<int>& temp){
+//         if(ind==nums.size()){
+//             s.insert(temp);
+//             return;
+//         }
+//         temp.push_back(nums[ind]);
+//         solve(ind+1,nums,temp);
+//         temp.pop_back();
+//         solve(ind+1,nums,temp);
+//         return;
+//     }
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         vector<int> temp;
+//         solve(0,nums,temp);
+//         vector<vector<int>> ans(s.begin(),s.end());
+//         return ans;
+//     }
+// };
+
+// better solution: prevent duplicates during generation
 class Solution {
 public:
-    set<vector<int>> s;
-    void solve(int ind, vector<int>& nums, vector<int>& temp){
-        if(ind==nums.size()){
-            s.insert(temp);
-            return;
+    vector<vector<int>> ans;
+    void solve(int start, vector<int>& nums, vector<int>& temp) {
+        ans.push_back(temp);
+        for (int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1])
+                continue;
+            temp.push_back(nums[i]);
+            solve(i + 1, nums, temp);
+            temp.pop_back();
         }
-        temp.push_back(nums[ind]);
-        solve(ind+1,nums,temp);
-        temp.pop_back();
-        solve(ind+1,nums,temp);
-        return;
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
         vector<int> temp;
-        solve(0,nums,temp);
-        vector<vector<int>> ans(s.begin(),s.end());
+        solve(0, nums, temp);
         return ans;
     }
 };
