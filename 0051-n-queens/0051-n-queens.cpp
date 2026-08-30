394e0833860b1e756a36b7b41960560f20a20d00
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-08-30 13:04:46 +0530

    Time: 1 ms (78.27%), Space: 12.3 MB (15.53%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<string> func(vector<pair<int,int>>& temp, int n){
+        vector<string> ans1;
+        for(auto it: temp){
+            string ans2(n,'.');
+            int col=it.second;
+            ans2[col]='Q';
+            ans1.push_back(ans2);
+        }
+        return ans1;
+    }
+    vector<vector<string>> ans;
+    void solve(int ind, int n, vector<pair<int,int>>& temp){
+        if(ind==n){
+            ans.push_back(func(temp,n));
+            return;
+        }
+        for(int i=0;i<n;i++){
+            bool flag=true;
+            for(auto it: temp){
+                int row=it.first;
+                int col=it.second;
+                if(i==col || abs(row-ind)==abs(col-i)){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                temp.push_back({ind,i});
+                solve(ind+1,n,temp);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<pair<int,int>> temp;
+        solve(0,n,temp);
+        return ans;
+    }
+};
