c64699cad5184993075a10818b091f8bcea53ad8
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-03 15:05:10 +0530

    Time: 0 ms (100%), Space: 10.7 MB (98.32%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,20 +9,50 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// RECURSIVE SOLUTION TC=O(N) SC=O(N)
+// class Solution {
+// public:
+//     void inorder(TreeNode* root, vector<int>& ans) {
+//         if(root == nullptr)
+//             return;
+
+//         inorder(root->left, ans);
+//         ans.push_back(root->val);
+//         inorder(root->right, ans);
+//     }
+
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         inorder(root, ans);
+//         return ans;
+//     }
+// };
+
+// MORRIS INORDER TRAVERSAL TC=O(N) SC=O(1)
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& ans) {
-        if(root == nullptr)
-            return;
-
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
-    }
-
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inorder(root, ans);
+        TreeNode* curr=root;
+        while(curr!=nullptr){
+            if(curr->left==nullptr){
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }else{
+                TreeNode* prev=curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==nullptr){
+                    prev->right=curr;
+                    curr=curr->left;
+                }else{
+                    prev->right=nullptr;
+                    ans.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
         return ans;
     }
 };
