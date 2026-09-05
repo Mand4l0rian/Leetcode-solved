f80df3edbf625775dc4bfc43ea964884cd9b3925
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-05 11:56:23 +0530

    Time: 11 ms (83.46%), Space: 18.9 MB (92.8%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -16,22 +16,44 @@ public:
 };
 */
 
+// LEVEL ORDER TRAVERSAL/ BFS TC=O(N) SC=O(N)
+// class Solution {
+// public:
+//     Node* connect(Node* root) {
+//         if(root==nullptr) return root;
+//         queue<Node*> q;
+//         q.push(root);
+//         while(!q.empty()){
+//             int n=q.size();
+//             for(int i=0;i<n;i++){
+//                 Node* temp=q.front();
+//                 q.pop();
+//                 if(i==n-1) temp->next=nullptr;
+//                 else temp->next=q.front();
+//                 if(temp->left) q.push(temp->left);
+//                 if(temp->right) q.push(temp->right);
+//             }
+//         }
+//         return root;
+//     }
+// };
+
+// OPTIMAL APPROACH TC=O(N) SC=O(1) EXPLOIT PERFECT BINARY TREE STRUCTURE
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(root==nullptr) return root;
-        queue<Node*> q;
-        q.push(root);
-        while(!q.empty()){
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                Node* temp=q.front();
-                q.pop();
-                if(i==n-1) temp->next=nullptr;
-                else temp->next=q.front();
-                if(temp->left) q.push(temp->left);
-                if(temp->right) q.push(temp->right);
+        if (root == nullptr) return nullptr;
+        Node* leftmost = root;
+        while (leftmost->left != nullptr) {
+            Node* curr = leftmost;
+            while (curr != nullptr) {
+                curr->left->next = curr->right;
+                if (curr->next != nullptr) {
+                    curr->right->next = curr->next->left;
+                }
+                curr = curr->next;
             }
+            leftmost = leftmost->left;
         }
         return root;
     }
