43ea88cc969a73032000fd31d61f1b5a4e9f6ad6
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-09 14:54:18 +0530

    Time: 0 ms (100%), Space: 10.7 MB (53.55%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -16,6 +16,28 @@
 // };
 
 // MEMOIZATION:
+// class Solution {
+// public:
+//     int solve(int ind, int& end, vector<int>& nums, vector<int>& dp){
+//         if(ind>=end) return 0;
+//         if(dp[ind]!=-1) return dp[ind];
+//         int take= nums[ind]+ solve(ind+2,end,nums,dp);
+//         int nottake= solve(ind+1,end,nums,dp);
+//         return dp[ind]=max(take,nottake);
+//     }
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         int n1=n-1;
+//         if(n==1) return nums[0];
+//         vector<int> dp(n,-1);
+//         vector<int> dp1(n1,-1);
+//         solve(0,n1,nums,dp1);
+//         solve(1,n,nums,dp);
+//         return max(dp1[0],dp[1]);
+//     }
+// };
+
+// TABULATION:
 class Solution {
 public:
     int solve(int ind, int& end, vector<int>& nums, vector<int>& dp){
@@ -27,12 +49,21 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int n1=n-1;
         if(n==1) return nums[0];
-        vector<int> dp(n,-1);
-        vector<int> dp1(n1,-1);
-        solve(0,n1,nums,dp1);
-        solve(1,n,nums,dp);
+        vector<int> dp(n+2,0);
+        vector<int> dp1(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            if(i!=n-1){
+                int take1=nums[i]+dp1[i+2];
+                int nottake1=dp1[i+1];
+                dp1[i]=max(take1,nottake1);
+            }
+            if(i!=0){
+                int take= nums[i]+dp[i+2];
+                int nottake=dp[i+1];
+                dp[i]=max(take,nottake);
+            }
+        }
         return max(dp1[0],dp[1]);
     }
 };
