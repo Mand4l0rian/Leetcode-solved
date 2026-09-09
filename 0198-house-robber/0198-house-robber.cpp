07c3496229bdf97ed2585c4b4b401650d35ed12a
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-09 14:06:45 +0530

    Time: 0 ms (100%), Space: 10.5 MB (64.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -13,18 +13,30 @@
 // };
 
 // MEMOIZATION:
+// class Solution {
+// public:
+//     int solve(int ind, vector<int>& nums, vector<int>& dp){
+//         if(ind>=nums.size()) return 0;
+//         if(dp[ind]!=-1) return dp[ind];
+//         int take= nums[ind]+ solve(ind+2,nums,dp);
+//         int nottake= solve(ind+1,nums,dp);
+//         return dp[ind]=max(take,nottake);
+//     }
+//     int rob(vector<int>& nums) {
+//         vector<int> dp(nums.size(),-1);
+//         solve(0,nums,dp);
+//         return dp[0];
+//     }
+// };
+
+// TABULATION:
 class Solution {
 public:
-    int solve(int ind, vector<int>& nums, vector<int>& dp){
-        if(ind>=nums.size()) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int take= nums[ind]+ solve(ind+2,nums,dp);
-        int nottake= solve(ind+1,nums,dp);
-        return dp[ind]=max(take,nottake);
-    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        solve(0,nums,dp);
+        vector<int> dp(nums.size()+2,0);
+        for(int i=nums.size()-1;i>=0;i--){
+            dp[i]=max(dp[i+1], nums[i]+dp[i+2]);
+        }
         return dp[0];
     }
 };
