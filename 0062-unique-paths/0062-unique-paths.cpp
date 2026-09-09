7cce840784189937df1d756043b272c3ddf410de
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-09 16:04:13 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -114,17 +114,32 @@
 // };
 
 // TABULATION:
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+//         dp[m-1][n-1]=1;
+//         for(int i=m-1;i>=0;i--){
+//             for(int j=n-1;j>=0;j--){
+//                 if(i==m-1 && j==n-1) continue;
+//                 dp[i][j]=dp[i+1][j]+dp[i][j+1];
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
+
+// SPACE OPTIMIZATION:
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-        dp[m-1][n-1]=1;
+        vector<int> dp(n+1,0);
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
-                if(i==m-1 && j==n-1) continue;
-                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                if(i==m-1 && j==n-1) dp[j]=1;
+                else dp[j]=dp[j]+dp[j+1];
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
