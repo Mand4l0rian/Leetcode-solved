61016269b5395b70778d057c16dc5b378f1908bf
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-09 12:57:57 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -8,18 +8,28 @@
 //     }
 // };
 
-//
+// MEMOIZATION:
 class Solution {
 public:
     int solve(int n, vector<int>& dp) {
         if(n<0) return 0;
         if(n==0) return 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        if(dp[n-1]!=-1) return dp[n-1];
+        return dp[n-1]=solve(n-1,dp)+solve(n-2,dp);
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n,-1);
         solve(n,dp);
-        return dp[n];
+        return dp[n-1];
     }
 };
+
+// TABULATION:
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n==0) return 1;
+//         else if(n<0) return 0;
+//         vector<int> dp(n+2);
+//     }
+// };
