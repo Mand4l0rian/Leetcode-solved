f2deb00622019d6e8faba7c8eaa376cda3655b58
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-09 13:39:08 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -25,14 +25,29 @@
 // };
 
 // TABULATION:
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         vector<int> dp(n+2,0);
+//         dp[1]=1;
+//         for(int i=2;i<n+2;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n+1];
+//     }
+// };
+
+// 
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+2,0);
-        dp[1]=1;
-        for(int i=2;i<n+2;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        int prev=0;
+        int prev1=1;
+        for(int i=1;i<n+1;i++){
+            int sum=prev1+prev;
+            prev=prev1;
+            prev1=sum;
         }
-        return dp[n+1];
+        return prev1;
     }
 };
