4d3226efd6d4b96d74033c78b0ef0b9c324dc194
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-09 14:37:05 +0530

    Time: 0 ms (100%), Space: 11 MB (18.59%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,38 @@
+// RECURSION:
+// class Solution {
+// public:
+//     int solve(int ind, int& end, vector<int>& nums){
+//         if(ind>=end) return 0;
+//         int take= nums[ind]+ solve(ind+2,end,nums);
+//         int nottake= solve(ind+1,end,nums);
+//         return max(take,nottake);
+//     }
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         int n1=n-1;
+//         if(n==1) return nums[0];
+//         return max(solve(0,n1,nums), solve(1,n,nums));
+//     }
+// };
+
+// MEMOIZATION:
+class Solution {
+public:
+    int solve(int ind, int& end, vector<int>& nums, vector<int>& dp){
+        if(ind>=end) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int take= nums[ind]+ solve(ind+2,end,nums,dp);
+        int nottake= solve(ind+1,end,nums,dp);
+        return dp[ind]=max(take,nottake);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        int n1=n-1;
+        if(n==1) return nums[0];
+        vector<int> dp(n,-1);
+        vector<int> dp1(n1,-1);
+        solve(0,n1,nums,dp1);
+        solve(1,n,nums,dp);
+        return max(dp1[0],dp[1]);
+    }
+};
