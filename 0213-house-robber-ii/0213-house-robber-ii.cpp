ae170fe492353574666f0867332af8a1c25f4c5c
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-09 14:59:35 +0530

    Time: 0 ms (100%), Space: 10.1 MB (88.44%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -38,25 +38,55 @@
 // };
 
 // TABULATION:
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==1) return nums[0];
+//         vector<int> dp(n+2,0);
+//         vector<int> dp1(n+1,0);
+//         for(int i=n-1;i>=0;i--){
+//             if(i!=n-1){
+//                 int take1=nums[i]+dp1[i+2];
+//                 int nottake1=dp1[i+1];
+//                 dp1[i]=max(take1,nottake1);
+//             }
+//             if(i!=0){
+//                 int take= nums[i]+dp[i+2];
+//                 int nottake=dp[i+1];
+//                 dp[i]=max(take,nottake);
+//             }
+//         }
+//         return max(dp1[0],dp[1]);
+//     }
+// };
+
+// SPACE OPTIMIZATION:
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
-        vector<int> dp(n+2,0);
-        vector<int> dp1(n+1,0);
+        int next=0;
+        int nnext=0;
+        int next1=0;
+        int nnext1=0;
         for(int i=n-1;i>=0;i--){
             if(i!=n-1){
-                int take1=nums[i]+dp1[i+2];
-                int nottake1=dp1[i+1];
-                dp1[i]=max(take1,nottake1);
+                int take1=nums[i]+nnext1;
+                int nottake1=next1;
+                int ans1=max(take1,nottake1);
+                nnext1=next1;
+                next1=ans1;
             }
             if(i!=0){
-                int take= nums[i]+dp[i+2];
-                int nottake=dp[i+1];
-                dp[i]=max(take,nottake);
+                int take= nums[i]+nnext;
+                int nottake=next;
+                int ans=max(take,nottake);
+                nnext=next;
+                next=ans;
             }
         }
-        return max(dp1[0],dp[1]);
+        return max(next,next1);
     }
 };
