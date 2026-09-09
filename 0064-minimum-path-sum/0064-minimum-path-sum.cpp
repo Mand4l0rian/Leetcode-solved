046512e6f72b52c8c8934b1e1eda176cd119e22c
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-09 17:10:49 +0530

    Time: 5 ms (6.53%), Space: 16.5 MB (15.91%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -103,17 +103,35 @@
 // };
 
 // MEMOIZATION
+// class Solution {
+// public:
+//     int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
+//         if(i==grid.size() || j==grid[0].size()) return INT_MAX;
+//         if(i==grid.size()-1 && j==grid[0].size()-1) return grid[i][j];
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int least= min(solve(i+1,j,grid,dp),solve(i,j+1,grid,dp));
+//         return dp[i][j]=grid[i][j]+least;
+//     }
+//     int minPathSum(vector<vector<int>>& grid) {
+//         vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
+//         return solve(0,0,grid,dp);
+//     }
+// };
+
+// TABULATION:
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
-        if(i==grid.size() || j==grid[0].size()) return INT_MAX;
-        if(i==grid.size()-1 && j==grid[0].size()-1) return grid[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int least= min(solve(i+1,j,grid,dp),solve(i,j+1,grid,dp));
-        return dp[i][j]=grid[i][j]+least;
-    }
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-        return solve(0,0,grid,dp);
+        vector<vector<int>> dp(grid.size()+1,vector<int>(grid[0].size()+1,INT_MAX));
+        for(int i=grid.size()-1;i>=0;i--){
+            for(int j=grid[0].size()-1;j>=0;j--){
+                if(i==grid.size()-1 && j==grid[0].size()-1){
+                    dp[i][j]= grid[i][j];
+                    continue;
+                }
+                dp[i][j]=grid[i][j]+min(dp[i+1][j],dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
