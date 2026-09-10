7b2896d11a4922690c3a9d8e86d3d9858467c4a7
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-10 19:48:51 +0530

    Time: 79 ms (50.97%), Space: 75.9 MB (45.88%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,58 @@
+// 1: tabulation
+// class Solution {
+// public:
+//     int longestPalindromeSubseq(string s) {
+//         int n=s.length();
+//         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n;j++){
+//                 if(s[i-1]==s[n-j]) dp[i][j]=1+dp[i-1][j-1];
+//                 else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+//             }
+//         }
+//         return dp[n][n];
+//     }
+// };
+
+// 2: space optimization
+// class Solution {
+// public:
+//     int longestPalindromeSubseq(string s) {
+//         int n=s.length();
+//         vector<int> dp(n+1,0);
+//         vector<int> temp(n+1,0);
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n;j++){
+//                 if(s[i-1]==s[n-j]) temp[j]=1+dp[j-1];
+//                 else temp[j]=max(dp[j],temp[j-1]);
+//             }
+//             dp=temp;
+//         }
+//         return dp[n];
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+// TABULATION:
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1,0));
+        for(int i=s.size()-1;i>=0;i--){
+            for(int j=s.size()-1;j>=0;j--){
+                if(s[i]==s[s.size()-1-j]) dp[i][j]=1+dp[i+1][j+1];
+                else dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
+    }
+};
