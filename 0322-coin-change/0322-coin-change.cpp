c6b58088457c000eb1f565c175d6c65229d9ac75
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-10 15:12:54 +0530

    Time: 51 ms (25.96%), Space: 53 MB (14.53%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -126,20 +126,40 @@
 // };
 
 // MEMOIZATION:
+// class Solution {
+// public:
+//     int solve(int ind, int amount, vector<int>& coins, vector<vector<int>>& dp) {
+//         if (amount == 0) return 0;
+//         if (ind == coins.size()) return 1e9;
+//         if(dp[ind][amount]!=-1) return dp[ind][amount];
+//         int take = 1e9;
+//         if (coins[ind] <= amount) take = 1 + solve(ind,amount-coins[ind],coins,dp);
+//         int notTake = solve(ind+1,amount,coins,dp);
+//         return dp[ind][amount]=min(take,notTake);
+//     }
+//     int coinChange(vector<int>& coins, int amount) {
+//         vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
+//         int ans = solve(0,amount,coins,dp);
+//         return ans >= 1e9 ? -1 : ans;
+//     }
+// };
+
+// TABULATION:
 class Solution {
 public:
-    int solve(int ind, int amount, vector<int>& coins, vector<vector<int>>& dp) {
-        if (amount == 0) return 0;
-        if (ind == coins.size()) return 1e9;
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int take = 1e9;
-        if (coins[ind] <= amount) take = 1 + solve(ind,amount-coins[ind],coins,dp);
-        int notTake = solve(ind+1,amount,coins,dp);
-        return dp[ind][amount]=min(take,notTake);
-    }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        int ans = solve(0,amount,coins,dp);
-        return ans >= 1e9 ? -1 : ans;
+        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,1e9));
+        for(int i=0;i<=coins.size();i++){
+            dp[i][0]=0;
+        }
+        for(int i=coins.size()-1;i>=0;i--){
+            for(int j=1;j<=amount;j++){
+                int take=1e9;
+                if(coins[i]<=j) take=1+dp[i][j-coins[i]];
+                int nottake=dp[i+1][j];
+                dp[i][j]=min(take,nottake);
+            }
+        }
+        return dp[0][amount]>=1e9?-1:dp[0][amount];
     }
 };
