fe20fdf1de8d8100f261d987bbd259da69a25c6d
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-11 16:23:56 +0530

    Time: 30 ms (66.59%), Space: 31.6 MB (38.78%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,57 @@
+// 1: tabulation
+// class Solution {
+// public:
+//     int minInsertions(string s) {
+//         int n=s.length();
+//         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n;j++){
+//                 if(s[i-1]==s[n-j]) dp[i][j]=1+dp[i-1][j-1];
+//                 else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+//             }
+//         }
+//         return n-dp[n][n];
+//     }
+// };
+
+// 2: space optimization
+// class Solution {
+// public:
+//     int minInsertions(string s) {
+//         int n=s.length();
+//         vector<int> dp(n+1,0);
+//         vector<int> temp(n+1,0);
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n;j++){
+//                 if(s[i-1]==s[n-j]) temp[j]=1+dp[j-1];
+//                 else temp[j]=max(dp[j],temp[j-1]);
+//             }
+//             dp=temp;
+//         }
+//         return n-dp[n];
+//     }
+// };
+
+
+
+
+
+
+
+
+// TABULATION:
+class Solution {
+public:
+    int minInsertions(string s) {
+        vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1,0));
+        for(int i=s.size()-1;i>=0;i--){
+            for(int j=s.size()-1;j>=0;j--){
+                if(s[i]==s[s.size()-1-j]) dp[i][j]=1+dp[i+1][j+1];
+                else{
+                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        return s.size()-dp[0][0];
+    }
+};
