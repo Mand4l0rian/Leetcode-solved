9269bdf94c3177ea82a63992a6f01933bb8eb2d6
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-11 19:08:58 +0530

    Time: 167 ms (6.44%), Space: 158.1 MB (5.17%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -15,19 +15,37 @@
 // };
 
 // MEMOIZATION:
+// class Solution {
+// public:
+//     int solve(int ind, bool canbuy, vector<int>& prices, vector<vector<int>>& dp){
+//         if(ind==prices.size()) return 0;
+//         if(dp[ind][canbuy]!=-1) return dp[ind][canbuy];
+//         if(canbuy){
+//             return dp[ind][canbuy]=max(-prices[ind]+solve(ind+1,!canbuy,prices,dp),solve(ind+1,canbuy,prices,dp));
+//         }else{
+//             return dp[ind][canbuy]=max(prices[ind],solve(ind+1,canbuy,prices,dp));
+//         }
+//     }
+//     int maxProfit(vector<int>& prices) {
+//         vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
+//         return solve(0,1,prices,dp);
+//     }
+// };
+
+// TABULATION:
 class Solution {
 public:
-    int solve(int ind, bool canbuy, vector<int>& prices, vector<vector<int>>& dp){
-        if(ind==prices.size()) return 0;
-        if(dp[ind][canbuy]!=-1) return dp[ind][canbuy];
-        if(canbuy){
-            return dp[ind][canbuy]=max(-prices[ind]+solve(ind+1,!canbuy,prices,dp),solve(ind+1,canbuy,prices,dp));
-        }else{
-            return dp[ind][canbuy]=max(prices[ind],solve(ind+1,canbuy,prices,dp));
-        }
-    }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
-        return solve(0,1,prices,dp);
+        vector<vector<int>> dp(prices.size()+1,vector<int>(2,0));
+        for(int i=prices.size()-1;i>=0;i--){
+            for(int j=1;j>=0;j--){
+                if(j==1){
+                    dp[i][j]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                }else{
+                    dp[i][j]=max(prices[i],dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
