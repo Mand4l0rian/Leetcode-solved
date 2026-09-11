c747a4ff0f3f12c8a0cb283ea7e63e46ab7b60f4
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-11 19:16:45 +0530

    Time: 19 ms (6.44%), Space: 97.4 MB (61.63%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -33,19 +33,39 @@
 // };
 
 // TABULATION:
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         vector<vector<int>> dp(prices.size()+1,vector<int>(2,0));
+//         for(int i=prices.size()-1;i>=0;i--){
+//             for(int j=1;j>=0;j--){
+//                 if(j==1){
+//                     dp[i][j]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+//                 }else{
+//                     dp[i][j]=max(prices[i],dp[i+1][j]);
+//                 }
+//             }
+//         }
+//         return dp[0][1];
+//     }
+// };
+
+// SPACE OPTIMIZATION:
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size()+1,vector<int>(2,0));
+        vector<int> dp(2,0);
+        vector<int> temp(2,0);
         for(int i=prices.size()-1;i>=0;i--){
             for(int j=1;j>=0;j--){
                 if(j==1){
-                    dp[i][j]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                    temp[j]=max(-prices[i]+dp[0],dp[1]);
                 }else{
-                    dp[i][j]=max(prices[i],dp[i+1][j]);
+                    temp[j]=max(prices[i],dp[j]);
                 }
             }
+            dp=temp;
         }
-        return dp[0][1];
+        return dp[1];
     }
 };
