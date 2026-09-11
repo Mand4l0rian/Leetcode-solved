46e6ff0619725b3a0a33d9a47be57747d7480e3a
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-11 18:09:57 +0530

    Time: 9 ms (38.32%), Space: 13.4 MB (32.34%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -43,17 +43,38 @@
 
 
 // RECURSION: REMEMBER THAT EVERYWHERE IN LCS PATTERN WHEN WE PERFORM RECURSION, WE DIRECTLY RETURN IN THE "TAKE" CASE WHICH IS CHARACTER MATCHING IN BOTH THE STRINGS, UNLIKE THE TRADITIONAL TAKE/NOTTAKE WHERE WE HAVE TO COMPARE TAKE AND NOTTAKE OUTPUTS IN THE FINAL RETURN STATEMENT OF THE FUNCTION.
+// class Solution {
+// public:
+//     int solve(int ind1, int ind2, string& s1, string& s2, vector<vector<int>>& dp){
+//         if(ind1==s1.size()) return s2.size()-ind2;
+//         if(ind2==s2.size()) return s1.size()-ind1;
+//         if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+//         if(s1[ind1]==s2[ind2]) return dp[ind1][ind2]=solve(ind1+1,ind2+1,s1,s2,dp);
+//         return dp[ind1][ind2]=1+min({solve(ind1+1,ind2,s1,s2,dp),solve(ind1+1,ind2+1,s1,s2,dp),solve(ind1,ind2+1,s1,s2,dp)});
+//     }
+//     int minDistance(string word1, string word2) {
+//         vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,-1));
+//         return solve(0,0,word1,word2,dp);
+//     }
+// };
+
+// TABULATION:
 class Solution {
 public:
-    int solve(int ind1, int ind2, string& s1, string& s2, vector<vector<int>>& dp){
-        if(ind1==s1.size()) return s2.size()-ind2;
-        if(ind2==s2.size()) return s1.size()-ind1;
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        if(s1[ind1]==s2[ind2]) return dp[ind1][ind2]=solve(ind1+1,ind2+1,s1,s2,dp);
-        return dp[ind1][ind2]=1+min({solve(ind1+1,ind2,s1,s2,dp),solve(ind1+1,ind2+1,s1,s2,dp),solve(ind1,ind2+1,s1,s2,dp)});
-    }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,-1));
-        return solve(0,0,word1,word2,dp);
+        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,0));
+        for(int j=0;j<word2.size();j++){
+            dp[word1.size()][j]=word2.size()-j;
+        }
+        for(int i=0;i<word1.size();i++){
+            dp[i][word2.size()]=word1.size()-i;
+        }
+        for(int i=word1.size()-1;i>=0;i--){
+            for(int j=word2.size()-1;j>=0;j--){
+                if(word1[i]==word2[j]) dp[i][j]= dp[i+1][j+1];
+                else dp[i][j]=1+min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]});
+            }
+        }
+        return dp[0][0];
     }
 };
