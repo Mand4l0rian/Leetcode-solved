39f35cb4de42f54ae4375512ceec8e8567db6416
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-11 20:16:22 +0530

    Time: 8 ms (52.22%), Space: 18.4 MB (28.23%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -104,19 +104,39 @@
 // };
 
 // MEMOIZATION:
+// class Solution {
+// public:
+//     int solve(int ind, int canbuy, int k, vector<int>& prices, vector<vector<vector<int>>>& dp){
+//         if(ind==prices.size() || k==0) return 0;
+//         if(dp[ind][canbuy][k]!=-1) return dp[ind][canbuy][k];
+//         if(canbuy){
+//             return dp[ind][canbuy][k]=max(-prices[ind]+solve(ind+1,!canbuy,k,prices,dp),solve(ind+1,canbuy,k,prices,dp));
+//         }else{
+//             return dp[ind][canbuy][k]=max(prices[ind]+solve(ind,!canbuy,k-1,prices,dp),solve(ind+1,canbuy,k,prices,dp));
+//         }
+//     }
+//     int maxProfit(int k, vector<int>& prices) {
+//         vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(2,vector<int>(k+1,-1)));
+//         return solve(0,1,k,prices,dp);
+//     }
+// };
+
+// TABULATION:
 class Solution {
 public:
-    int solve(int ind, int canbuy, int k, vector<int>& prices, vector<vector<vector<int>>>& dp){
-        if(ind==prices.size() || k==0) return 0;
-        if(dp[ind][canbuy][k]!=-1) return dp[ind][canbuy][k];
-        if(canbuy){
-            return dp[ind][canbuy][k]=max(-prices[ind]+solve(ind+1,!canbuy,k,prices,dp),solve(ind+1,canbuy,k,prices,dp));
-        }else{
-            return dp[ind][canbuy][k]=max(prices[ind]+solve(ind,!canbuy,k-1,prices,dp),solve(ind+1,canbuy,k,prices,dp));
-        }
-    }
     int maxProfit(int k, vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return solve(0,1,k,prices,dp);
+        vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        for(int i=prices.size()-1;i>=0;i--){
+            for(int j=1;j>=0;j--){
+                for(int n=1;n<=k;n++){
+                    if(j){
+                        dp[i][j][n]=max(-prices[i]+dp[i+1][0][n],dp[i+1][1][n]);
+                    }else{
+                        dp[i][j][n]=max(prices[i]+dp[i][1][n-1],dp[i+1][0][n]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][k];
     }
 };
