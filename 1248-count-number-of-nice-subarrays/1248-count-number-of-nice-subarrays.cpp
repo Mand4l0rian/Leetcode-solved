9e79f85c1ec877acb27eba2666448fbf9d74c838
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-15 19:40:42 +0530

    Time: 6 ms (70.66%), Space: 71.6 MB (47.17%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,26 +1,58 @@
+// PREFIX SUM: NOT SPACE OPTIMAL
+// class Solution {
+// public:
+//     int numberOfSubarrays(vector<int>& nums, int k) {
+//         unordered_map<int, int> mp;
+
+//         // 0 odd numbers before the array
+//         mp[0] = 1;
+
+//         int oddCount = 0;
+//         int ans = 0;
+
+//         for (int num : nums) {
+//             if (num % 2 == 1)
+//                 oddCount++;
+
+//             // Need previous prefix with:
+//             // previousOddCount = oddCount - k
+//             if (mp.count(oddCount - k))
+//                 ans += mp[oddCount - k];
+
+//             mp[oddCount]++;
+//         }
+
+//         return ans;
+//     }
+// };
+
+// SLIDING WINDOW: OPTIMAL
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-
-        // 0 odd numbers before the array
-        mp[0] = 1;
-
-        int oddCount = 0;
+    int atMost(vector<int>& nums, int k) {
+        int left = 0;
+        int odds = 0;
         int ans = 0;
 
-        for (int num : nums) {
-            if (num % 2 == 1)
-                oddCount++;
+        for (int right = 0; right < nums.size(); right++) {
 
-            // Need previous prefix with:
-            // previousOddCount = oddCount - k
-            if (mp.count(oddCount - k))
-                ans += mp[oddCount - k];
+            if (nums[right] % 2 == 1)
+                odds++;
 
-            mp[oddCount]++;
+            while (odds > k) {
+                if (nums[left] % 2 == 1)
+                    odds--;
+
+                left++;
+            }
+
+            ans += right - left + 1;
         }
 
         return ans;
+    }
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
     }
 };
