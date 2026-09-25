deafdb42cf95d63a7051f91f8f19a224fa355bba
Mand4l0rian <143513369+Mand4l0rian@users.noreply.github.com>
2026-09-25 16:46:52 +0530

    Time: 522 ms (8.22%), Space: 9 MB (30.54%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,48 +1,50 @@
 class Solution {
 public:
-    bool row[9][10] = {};
-    bool col[9][10] = {};
-    bool box[9][10] = {};
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
 
     bool solve(vector<vector<char>>& board) {
-        for(int r = 0; r < 9; r++) {
-            for(int c = 0; c < 9; c++) {
-                if(board[r][c] != '.') continue;
-                for(int x = 1; x <= 9; x++) {
-                    int b = (r / 3) * 3 + (c / 3);
-                    if(!row[r][x] && !col[c][x] && !box[b][x]) {
-                        board[r][c] = '0' + x;
-                        row[r][x] = true;
-                        col[c][x] = true;
-                        box[b][x] = true;
-                        if(solve(board))
-                            return true;
-                        // Backtrack
-                        board[r][c] = '.';
-                        row[r][x] = false;
-                        col[c][x] = false;
-                        box[b][x] = false;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+
+                if (board[i][j] == '.') {
+
+                    for (char c = '1'; c <= '9'; c++) {
+
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+
+                            if (solve(board) == true)
+                                return true;
+                            else
+                                board[i][j] = '.';
+                        }
                     }
+
+                    return false;
                 }
-                return false;
             }
         }
+
         return true;
     }
-    
-    void solveSudoku(vector<vector<char>>& board) {
-        // Initialize the tracking arrays
-        for(int r = 0; r < 9; r++) {
-            for(int c = 0; c < 9; c++) {
-                if(board[r][c] != '.') {
-                    int x = board[r][c] - '0';
-                    int b = (r / 3) * 3 + (c / 3);
-                    row[r][x] = true;
-                    col[c][x] = true;
-                    box[b][x] = true;
-                }
-            }
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+
+        for (int i = 0; i < 9; i++) {
+
+            if (board[i][col] == c)
+                return false;
+
+            if (board[row][i] == c)
+                return false;
+
+            if (board[3 * (row / 3) + i / 3]
+                    [3 * (col / 3) + i % 3] == c)
+                return false;
         }
-        solve(board);
+
+        return true;
     }
 };
